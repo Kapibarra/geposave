0029b6e7ec829a8be3e095601646d7280600514e
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-13 12:54:17 +0300

    added hero parallax

--- src/app/homepage/shared/components/hero/hero.component.ts
+++ src/app/homepage/shared/components/hero/hero.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { getAttrsForDirectiveMatching } from '@angular/compiler/src/render3/view/util';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  HostListener,
+  OnInit,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
-  styleUrls: ['./hero.component.scss']
+  styleUrls: ['./hero.component.scss'],
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements OnInit, AfterViewInit {
+  constructor() {}
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(event: any) {
+  console.log(event);
+  
+    this.parallaxLayer.forEach(layer => {
+      const speed:any = layer.nativeElement.getAttribute('data-speed');
+      const x = (window.innerWidth - event.pageX*speed)/100  ;
+      const y = (window.innerHeight - event.pageY*speed)/100;  
+  
+      layer.nativeElement.style.transform = `translateX(${x}px) translateY(${y}px)`
 
-  constructor() { }
+    })
+    
+    
+  }
+  @ViewChildren('parallaxLayer')
+  public parallaxLayer!: QueryList<ElementRef<HTMLLIElement>>;
 
   ngOnInit(): void {
   }
-
+  ngAfterViewInit() {
+     
+  }
 }
