49f28f0def0c9aa1fd2ebb85307b9227f699b60b
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-13 20:55:18 +0300

    fixes

--- src/app/homepage/shared/components/header/header.component.ts
+++ src/app/homepage/shared/components/header/header.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
-
+export class HeaderComponent implements OnInit, DoCheck {
+  mobile = false;
   constructor() { }
 
   ngOnInit(): void {
+    if (window.screen.width <= 968) {
+      // 768px portrait
+      this.mobile = true;
+      console.log('mobile', this.mobile);
+    }
   }
 
+  ngDoCheck() {
+    if (window.screen.width <= 968) {
+//resizeable
+      this.mobile = true;
+      console.log('mobile', this.mobile);
+    } else {
+      this.mobile = false;
+      console.log('false', this.mobile);
+    }
+  }
 }
