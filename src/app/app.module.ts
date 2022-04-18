dcf07cc8ddbd788747b1026db0dc4e51fadd3fb8
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-04-18 16:26:12 +0300

    init swiper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -43,7 +44,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     RouterModule,
-    HttpClientModule
+    HttpClientModule,
+    SwiperModule,
   ],
   providers: [AnimationData],
   bootstrap: [AppComponent]
